56b5982c741dbbbcbe12aa7e3b4ceba8a7497bb5
Isabelle BLASQUEZ <isabelle.blasquez31@gmail.com>
2017-11-19 15:51:47 +0100

    ajout ressources kata gilded rose

---
+++ ressources/gildedrose/src/test/java/fr/unilim/iut/gildedrose/InnTest.java
@@ -0,0 +1,10 @@
+package fr.unilim.iut.gildedrose;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InnTest {
+  @Test
+  public void should_write_tests() {
+  }
+}
